4f73dcc36396e4ff8835cf9857f43fd252664598
6M2Y <minyahel.yimer@gmail.com>
2024-11-07 11:15:10 +0100

    started with react-router-dom commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import  Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Final project</h1>
+      <Navbar/>
     </div>
   );
 }
