399c805bfc741ad3485687aae2883e7be026b9b3
Kristojh <30958753+Kristojh@users.noreply.github.com>
2024-11-11 20:13:06 +0100

    Updated Navbar.tsx

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,34 +1,23 @@
-import React, { useState } from "react";
-import { NavLink, Outlet, Link } from "react-router-dom";
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 import './Navbar.css';
-
 
 const Navbar = () => {
   return (
-    <>
-      <nav className="navbar">
-        {/* Left side - Logo */}
-        <div className="navbar-left">
-          <Link to={"/"}>
-            <img src="your-logo.png" alt="logo" />
-          </Link>
-        </div>
-
-        {/* Centered Search Input */}
-        <div className="navbar-center">
-          <input type="text" placeholder="Search" />
-        </div>
-
-        {/* Right side - Navigation Links */}
-        <div className="navbar-right">
-          <NavLink to={"/createPost"}>Write</NavLink>
-          <NavLink to={"/signIn"}>SignIn</NavLink>
-          <NavLink to={"/signUp"}>SignUp</NavLink>
-        </div>
-      </nav>
-      <Outlet />
-    </>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/" className="logo">SuperHeroes</Link>
+      </div>
+      <div className="navbar-center">
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="navbar-right">
+        <NavLink to="/createpost">Write</NavLink>
+        <NavLink to="/signin">Sign In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </div>
+    </nav>
   );
 };
 
-export default Navbar;
+export default Navbar;;
