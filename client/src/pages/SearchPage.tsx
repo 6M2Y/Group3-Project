4c4f76c4d1fae6789d1dc0ecb641e0f49131f9c7
6M2Y <minyahel.yimer@gmail.com>
2024-11-19 11:03:44 +0100

    search by innhold done

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -10,90 +10,9 @@ import {
   FaEye,
   FaCheckCircle,
 } from "react-icons/fa";
+import LeftSidebar from "../Components/LeftSidebar";
+import RightSideBar from "../Components/RightSideBar";
 
-// Define the Post interface based on the API response structure
-
-// const SearchPage: React.FC = () => {
-//   const { query } = useParams(); // Get the query from the route
-//   const [searchResults, setSearchResults] = useState<Post[]>([]);
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const fetchSearchResults = async () => {
-//       try {
-//         setLoading(true);
-//         setError(null);
-
-//         const response = await axios.post<{ wikiPost: Post[] }>(
-//           `${process.env.REACT_APP_WIKI_API_URL}/searchByTag`,
-//           { tag: query }
-//         );
-
-//         setSearchResults(response.data.wikiPost || []);
-//       } catch (err: any) {
-//         setError(
-//           err.response?.data?.error || "Failed to fetch search results."
-//         );
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     if (query) {
-//       fetchSearchResults();
-//     }
-//   }, [query]);
-
-//   return (
-//     <div>
-//       <h1>Search Results for: {query}</h1>
-//       {loading && <p>Loading...</p>}
-//       {error && <p className="error">{error}</p>}
-
-//       {searchResults.length > 0 ? (
-//         searchResults.map((post) => (
-//           <div key={post._id} className="post-result">
-//             <div className="post-item">
-//               <img
-//                 src={post.image}
-//                 alt={post.title}
-//                 className="post-image"
-//                 style={{ width: "150px", height: "150px", objectFit: "cover" }}
-//               />
-//               <div className="post-details">
-//                 <h3>{post.title}</h3>
-//                 <div className="author-info">
-//                   <p>
-//                     <strong>Author:</strong> {post.author.fullname}
-//                   </p>
-//                   <p>
-//                     <strong>Email:</strong> {post.author.email}
-//                   </p>
-//                 </div>
-//                 <div className="tags">
-//                   <strong>Tags:</strong> {post.tags.join(", ")}
-//                 </div>
-//                 <div className="views-and-status">
-//                   <p>
-//                     <strong>Views:</strong> {post.views}
-//                   </p>
-//                   <p>
-//                     <strong>Published:</strong> {post.published ? "Yes" : "No"}
-//                   </p>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         ))
-//       ) : (
-//         <p>No results found.</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default SearchPage;
 const SearchPage: React.FC = () => {
   const { query } = useParams();
   const [searchResults, setSearchResults] = useState<PostSearch[]>([]);
@@ -129,7 +48,9 @@ const SearchPage: React.FC = () => {
 
   const handlePostClick = async (post: PostSearch) => {
     try {
-      await axios.get(`http://localhost:4000/pages/${post._id}`);
+      await axios.get(
+        `${process.env.REACT_APP_WIKI_API_URL}/pages/${post._id}`
+      );
       navigate(`/postpage/${post._id}`, {
         state: { post },
       });
@@ -139,67 +60,66 @@ const SearchPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Search Results for: {query}</h1>
-      {loading && <p>Loading...</p>}
-      {error && <p className="error">{error}</p>}
+    <div className="main-content">
+      <LeftSidebar />
+      <div className="post-section search">
+        <h1>Search Results for: {query}</h1>
+        {loading && <p>Loading...</p>}
+        {error && <p className="error">{error}</p>}
 
-      {searchResults.length > 0 ? (
-        searchResults.map((post) => (
-          <div
-            key={post._id}
-            className="post-result"
-            onClick={() => handlePostClick(post)}
-          >
-            <div className="post-item">
-              <img
-                src={post.image}
-                alt={post.title}
-                className="post-image"
-                style={{ width: "150px", height: "150px", objectFit: "cover" }}
-              />
-              <div className="post-details">
-                <h3>{post.title}</h3>
-                <div className="details-inline">
-                  {/* Author */}
-                  <div className="author-info">
-                    <FaUser />
-                    <span>{post.author.fullname}</span>
+        {searchResults.length > 0 ? (
+          searchResults.map((post) => (
+            <div
+              key={post._id}
+              className="post-result"
+              onClick={() => handlePostClick(post)}
+            >
+              <div className="post-item">
+                <img src={post.image} alt={post.title} className="post-image" />
+                <div className="post-details">
+                  <h3>{post.title}</h3>
+                  <div className="details-inline">
+                    {/* Author */}
+                    <div className="author-info">
+                      <FaUser />
+                      <span>{post.author.fullname}</span>
+                    </div>
+
+                    {/* Email */}
+                    <div className="email-info">
+                      <FaEnvelope />
+                      <span>{post.author.email}</span>
+                    </div>
                   </div>
 
-                  {/* Email */}
-                  <div className="email-info">
-                    <FaEnvelope />
-                    <span>{post.author.email}</span>
-                  </div>
+                  <div className="details-inline">
+                    {/* Tags */}
+                    <div className="tags-info">
+                      <FaTag />
+                      <span>{post.tags.join(", ")}</span>
+                    </div>
 
-                  {/* Tags */}
-                  <div className="tags-info">
-                    <FaTag />
-                    <span>{post.tags.join(", ")}</span>
-                  </div>
-                </div>
+                    {/* Views */}
+                    <div className="views-info">
+                      <FaEye />
+                      <span>{post.views}</span>
+                    </div>
 
-                <div className="views-and-status">
-                  {/* Views */}
-                  <div>
-                    <FaEye />
-                    <span>{post.views}</span>
-                  </div>
-
-                  {/* Published Status */}
-                  <div>
-                    <FaCheckCircle />
-                    <span>{post.published ? "Yes" : "No"}</span>
+                    {/* Published Status */}
+                    <div className="published-info">
+                      <FaCheckCircle />
+                      <span>{post.published ? "Yes" : "No"}</span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))
-      ) : (
-        <p>No results found.</p>
-      )}
+          ))
+        ) : (
+          <p>No results found.</p>
+        )}
+      </div>
+      <RightSideBar />
     </div>
   );
 };
