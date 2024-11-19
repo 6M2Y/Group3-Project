a4c9f85b98c737cf7a0808d2f53c92b7e7ef2e29
6M2Y <minyahel.yimer@gmail.com>
2024-11-19 16:20:38 +0100

    updated the latestpost

--- client/src/Components/latestPostCard.tsx
+++ client/src/Components/latestPostCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../Styles/MainContent.css";
 import { latestPostType } from "../Common/interfaces";
+import { FaUser, FaCalendarAlt, FaTag } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const LatestPostCard = ({ content }: { content: latestPostType }) => {
   let { title, tags, updatedAt, author } = content;
@@ -8,12 +10,22 @@ const LatestPostCard = ({ content }: { content: latestPostType }) => {
     <li className="latest-post">
       <p className="latest-post-title">{title}</p>
       <div className="author-info">
-        <p className="latest-post-author">{author.fullname}</p>
-        <p className="latest-post-email">{author.email}</p>
+        <p className="latest-post-author">
+          <FaUser size={10} color="rgb(255, 87, 34)" /> {author.fullname}
+        </p>
+        <p className="latest-post-email">
+          {" "}
+          <MdEmail size={10} color="rgb(255, 87, 34)" />
+          {author.email}
+        </p>
       </div>
 
-      <p className="latest-post-tags">Tags: {tags.join(", ")}</p>
+      <p className="latest-post-tags">
+        <FaTag size={10} color="rgb(255, 87, 34)" /> Tags: {tags.join(", ")}
+      </p>
       <p className="latest-post-date">
+        {" "}
+        <FaCalendarAlt size={10} color="rgb(255, 87, 34)" />
         Updated:{" "}
         {new Date(updatedAt).toLocaleString("en-GB", {
           day: "2-digit",
